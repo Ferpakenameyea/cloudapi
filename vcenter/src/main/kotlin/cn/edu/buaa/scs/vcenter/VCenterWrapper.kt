52efd926e8eb886dccad56016ed1ad41a391acdd
ferpakenameyea <2834852759@qq.com>
2026-04-07 13:24:00 +0800

    remove disk restriction

--- vcenter/src/main/kotlin/cn/edu/buaa/scs/vcenter/VCenterWrapper.kt
+++ vcenter/src/main/kotlin/cn/edu/buaa/scs/vcenter/VCenterWrapper.kt
@@ -290,9 +290,6 @@ object VCenterWrapper {
                 for (ds in dataStores) {
                     val datastoreSummary = getMoRef.entityProps(ds, "summary")["summary"]!! as DatastoreSummary
                     println(datastoreSummary.name)
-                    if (datastoreSummary.name == "Dell SCV101") {
-                        continue
-                    }
                     if (datastoreSummary.isAccessible && datastoreSummary.freeSpace / (1024 * 1024) > 1024 * 1024 && datastoreSummary.freeSpace > diskRequired) {
                         host = hostRef
                         datastore = datastoreSummary.datastore
