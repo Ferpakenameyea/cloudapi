f3e664a4a73c58c6a178272846677af56f68f81b
ferpakenameyea <2834852759@qq.com>
2026-05-06 17:53:24 +0800

    fix harbor serialization issue

--- cloudapi-web/src/main/kotlin/cn/edu/buaa/scs/sdk/harbor/infrastructure/Serializer.kt
+++ cloudapi-web/src/main/kotlin/cn/edu/buaa/scs/sdk/harbor/infrastructure/Serializer.kt
@@ -1,6 +1,7 @@
 package cn.edu.buaa.scs.sdk.harbor.infrastructure
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
@@ -15,4 +16,5 @@ object Serializer {
         .findAndRegisterModules()
         .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 }
